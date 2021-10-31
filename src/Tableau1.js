99f1e1d3fe0c163743c2169a91986a4a8812dcd8
Kyliann <92451561+NanoLoriginal@users.noreply.github.com>
2021-10-31 13:12:35 +0100

    inclinaison de l'arbre

--- src/Tableau1.js
+++ src/Tableau1.js
@@ -84,7 +84,7 @@ class Tableau1 extends Phaser.Scene{
 
         let bg2Tree3=this.add.image(750,-100, 'bg2-tree-3').setOrigin(0,0);
         this.bg2Container.add(bg2Tree3);
-        bg2Tree3.angle=-5; //pencher l'arbre de -5 degrès
+        bg2Tree3.angle=-10; //pencher l'arbre de -5 degrès
 
 
 
